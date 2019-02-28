6728afa8ef2a0a0bd20ce8f2c9719f990bcd2b0c
GABOCALVO\Gabriel <gabriel.calvo.vargas@gmail.com>
2019-02-27 19:31:23 -0600

    primer test done

--- src/main/Analytics.java
+++ src/main/Analytics.java
@@ -1,5 +1,34 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Analytics {
+
+	public static String[] masLarga(String text) {
+		int max = 0;
+		String[] parts = text.split(" ");
+		ArrayList<String> list = new ArrayList();
+
+		for (String x : parts) {
+			if (x.length() >= max) {
+				max = x.length();
+			}
+		}
+
+		for (String x : parts) {
+			if (x.length() == max) {
+				list.add(x);
+			}
+		}
+
+		int cantidad = list.size();
+		String[] myList = new String[cantidad];
+
+		for (int i = 0; i < cantidad; i++) {
+			myList[i] = list.get(i);
+		}
+
+		return myList;
+	}
 
 }
--- src/test/TestAnalytics.java
+++ src/test/TestAnalytics.java
@@ -1,5 +1,26 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import main.Analytics;
+
 public class TestAnalytics {
+
+	@Test
+	public void testMasLarga() {
+
+		String text1 = "casa casita casototota";
+		String text2 = "casa casita casota";
+
+		String[] test1 = Analytics.masLarga(text1);
+		String[] test2 = Analytics.masLarga(text2);
+
+		assertEquals("[casototota]", Arrays.toString(test1), "It didn't bring the right string");
+		assertEquals("[casita, casota]", Arrays.toString(test2), "It didn't bring the right string");
+	}
 
 }
