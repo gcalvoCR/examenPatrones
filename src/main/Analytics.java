0f0d24db4d0b5ea8a7bc4dfedf38df937df8e9b3
GABOCALVO\Gabriel <gabriel.calvo.vargas@gmail.com>
2019-02-27 19:45:06 -0600

    segundo test done

--- src/main/Analytics.java
+++ src/main/Analytics.java
@@ -7,7 +7,7 @@ public class Analytics {
 	public static String[] masLarga(String text) {
 		int max = 0;
 		String[] parts = text.split(" ");
-		ArrayList<String> list = new ArrayList();
+		ArrayList<String> list = new ArrayList<>();
 
 		for (String x : parts) {
 			if (x.length() >= max) {
@@ -31,4 +31,40 @@ public class Analytics {
 		return myList;
 	}
 
+	public static String[] masCorta(String text) {
+		int min = 1000;
+		String[] myList;
+		if (text.length() == 0) {
+			myList = new String[1];
+			myList[0] = "";
+
+		} else {
+
+			String[] parts = text.split(" ");
+			ArrayList<String> list = new ArrayList<>();
+
+			for (String x : parts) {
+				if (x.length() < min) {
+					min = x.length();
+				}
+			}
+
+			for (String x : parts) {
+				if (x.length() == min) {
+					list.add(x);
+				}
+			}
+			System.out.println(list.toString());
+
+			int cantidad = list.size();
+			myList = new String[cantidad];
+
+			for (int i = 0; i < cantidad; i++) {
+				myList[i] = list.get(i);
+			}
+
+		}
+		return myList;
+
+	}
 }
--- src/test/TestAnalytics.java
+++ src/test/TestAnalytics.java
@@ -23,4 +23,17 @@ public class TestAnalytics {
 		assertEquals("[casita, casota]", Arrays.toString(test2), "It didn't bring the right string");
 	}
 
+	@Test
+	public void testMasCorta() {
+
+		String text1 = "casa casita casototota";
+		String text2 = "casa mia cosa ala";
+
+		String[] test1 = Analytics.masCorta(text1);
+		String[] test2 = Analytics.masCorta(text2);
+
+		assertEquals("[casa]", Arrays.toString(test1), "It didn't bring the right string");
+		assertEquals("[mia, ala]", Arrays.toString(test2), "It didn't bring the right string");
+	}
+
 }
